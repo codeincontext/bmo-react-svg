6cbe112c7f9f9457c15bb8adb595b3d9f8853d87
Adam Howard <skattyadz+gh@gmail.com>
2015-08-24 17:10:22 +0100

    add tablet buttons and basic screen

--- src/Bmo.jsx
+++ src/Bmo.jsx
@@ -11,6 +11,9 @@ var Bmo = React.createClass({
         width: 550,
         height: 800,
         color: '#6BBEB0'
+      },
+      tablet: {
+        height: 15
       }
     };
   },
@@ -18,6 +21,8 @@ var Bmo = React.createClass({
   render: function() {
     var body = this.props.body;
     var viewBox = [0, 0, body.width, body.height].join(' ');
+
+    var tabletHeight = 15;
 
     return (
       <svg xmlns="http://www.w3.org/svg/2000"
@@ -27,17 +32,25 @@ var Bmo = React.createClass({
 
         <rect x="0" y="0" width={body.width} height={body.height} rx="15" ry="15" fill={body.color}/>
 
+        <path d="M30 30 h 490 v 360 h -490 Z" fill="#D3FBE5" strokeWidth="3" stroke="black"/>
+
+        <rect x="70" y="430" width="300" height="25" fill="#133C36" strokeWidth="3" stroke="black"/>
+        <circle cx="440" cy="442" r="15" fill="#2320D1" strokeWidth="3" stroke="black"/>
+
+        <rect x="70" y="700" width="90" height={this.props.tablet.height} rx={this.props.tablet.height / 2} ry={this.props.tablet.height / 2} fill="#2320D1" strokeWidth="3" stroke="black"/>
+        <rect x="190" y="700" width="90" height={this.props.tablet.height} rx={this.props.tablet.height / 2} ry={this.props.tablet.height / 2} fill="#2320D1" strokeWidth="3" stroke="black"/>
+
         {ShapeHelpers.pseudo3dShape(
-          <circle cx="375" cy="630" r="50" fill="#F72A6A" strokeWidth="3" stroke="black"/>
+          <circle cx="375" cy="680" r="50" fill="#F72A6A" strokeWidth="3" stroke="black"/>
         )}
         {ShapeHelpers.pseudo3dShape(
-          <circle cx="460" cy="550" r="30" fill="#48F464" strokeWidth="3" stroke="black"/>
+          <circle cx="460" cy="600" r="30" fill="#48F464" strokeWidth="3" stroke="black"/>
         )}
         {ShapeHelpers.pseudo3dShape(
-          <Cross cx={100} cy={450} scale={50} fill="#FCDE5D"/>
+          <Cross cx={100} cy={500} scale={50} fill="#FCDE5D"/>
         )}
         {ShapeHelpers.pseudo3dShape(
-          <Triangle cx={320} cy={450} scale={20} fill="#47E1F2"/>
+          <Triangle cx={320} cy={500} scale={20} fill="#47E1F2"/>
         )}
       </svg>
     );
