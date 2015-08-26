5db4f4fecb571383f91f2f437527d19d8ac27560
Adam Howard <skattyadz+gh@gmail.com>
2015-08-26 15:39:03 +0100

    add basic face

--- src/Bmo.jsx
+++ src/Bmo.jsx
@@ -1,7 +1,8 @@
 var React = require('react'),
     Cross = require('./cross'),
     Triangle = require('./triangle'),
-    ShapeHelpers = require('./shapeHelpers');
+    ShapeHelpers = require('./shapeHelpers')
+    Face = require('./face');
 
 var Bmo = React.createClass({
 
@@ -22,22 +23,25 @@ var Bmo = React.createClass({
     var body = this.props.body;
     var viewBox = [0, 0, body.width, body.height].join(' ');
 
-    var tabletHeight = 15;
-
     return (
       <svg xmlns="http://www.w3.org/svg/2000"
         viewBox={viewBox}
         width={body.width}
         height={body.height}>
 
+        // body
         <rect x="0" y="0" width={body.width} height={body.height} rx="40" ry="40" fill={body.color}/>
 
+        // screen
         <rect x="58" y="58" width="434" height="314" rx="45" ry="45" fill="#D3FBE5" strokeWidth="6" stroke="#B3CFBE"/>
         <rect x="55" y="55" width="440" height="320" rx="45" ry="45" fill="transparent" strokeWidth="3" stroke="black"/>
+        <Face x={0} y={180} width={this.props.body.width} eyeDistance={230} eyeRadius={17} mouthWidth={100} mouthY={35} mouthHeight={60} color="black"/>
 
+        // tray
         <rect x="70" y="410" width="300" height="25" fill="#133C36" strokeWidth="3" stroke="black"/>
         <circle cx="440" cy="423" r="15" fill="#2320D1" strokeWidth="3" stroke="black"/>
 
+        // tablet buttons
         <rect x="70" y="710" width="90" height={this.props.tablet.height} rx={this.props.tablet.height / 2} ry={this.props.tablet.height / 2} fill="#2320D1" strokeWidth="3" stroke="black"/>
         <rect x="190" y="710" width="90" height={this.props.tablet.height} rx={this.props.tablet.height / 2} ry={this.props.tablet.height / 2} fill="#2320D1" strokeWidth="3" stroke="black"/>
 
---
+++ src/Face.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+
+var Face = React.createClass({
+
+  render: function() {
+    var eyeOneX = this.props.width/2 - this.props.eyeDistance/2;
+    var eyeTwoX = this.props.width/2 + this.props.eyeDistance/2;
+
+    var mouthStart = [this.props.width/2 - this.props.mouthWidth/2, this.props.mouthY].join(' ');
+    var mouthEnd = [this.props.mouthWidth, 0].join(' ');
+
+    var mouthPath = `M ${mouthStart} c 0 ${this.props.mouthHeight}, ${this.props.mouthWidth} ${this.props.mouthHeight}, ${mouthEnd}`;
+
+    return (
+      <g transform={`translate(${this.props.x}, ${this.props.y})`}>
+        <circle cx={eyeOneX} cy="0" r={this.props.eyeRadius} fill={this.props.color} strokeWidth="3" stroke={this.props.color}/>
+        <circle cx={eyeTwoX} cy="0" r={this.props.eyeRadius} fill={this.props.color} strokeWidth="3" stroke={this.props.color}/>
+
+        <path d={mouthPath} fill="transparent" stroke={this.props.color} strokeWidth="7" strokeLinecap="round" stroke={this.props.color}/>
+      </g>
+    )
+  }
+
+});
+
+module.exports = Face;
